3e1fbf2ee685f7be9c61217d37ad075550415925
lokesh <lokeshkhati005@gmail.com>
2023-06-05 12:14:39 +0530

    refactor

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 
 import { config } from "dotenv"
-import app from "./app.js"
-import { connectWithDB } from './config/db.js'
+import app from "./src/app.js"
+import { connectWithDB } from './src/config/db.js'
 
 config()
 connectWithDB()
